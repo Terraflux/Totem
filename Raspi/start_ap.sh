500b9a2c2a0876421c7c7c66fcfb6eefeb8d6724
c <snakecharmr1024@gmail.com>
2015-11-04 23:16:24 -0800

    80% hardware integration complete (made up number)

--- Raspi/start_ap.sh
+++ Raspi/start_ap.sh
@@ -1,6 +1,32 @@
-service hostapd restart
-service dnsmasq restart
+#!/bin/bash
+IPT=iptables
+LOCALHOST=10.0.19.84
+TOTEM_LOCALHOST=10.0.0.1
+AP_INTERFACE=wlan4
+#ifconfig $AP_INTERFACE down up $LOCALHOST
 
-HTTP_PORT=8080
 
-iptables -t nat -A PREROUTING --in-interface wlan0 --protocol tcp -j DNAT --to-destination $TOTEM_LOCALHOST:$HTTP_PORT
+#service hostapd restart
+#service dnsmasq restart
+
+HTTP_PORT=3000
+
+
+
+iptables -F
+iptables -X
+iptables -t nat -F
+iptables -t nat -X
+
+# Accept everything by default
+#$IPT -t nat --policy PREROUTING ACCEPT
+#$IPT -t nat --policy POSTROUTING ACCEPT
+#$IPT -t nat --policy OUTPUT ACCEPT
+
+#$IPT -A FORWARD --protocol ALL --in-interface $AP_INTERFACE -j ACCEPT
+#$IPT -A FORWARD --in-interface $AP_INTERFACE -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+
+iptables -t nat -A PREROUTING --in-interface $AP_INTERFACE --protocol udp -j DNAT --to-destination $LOCALHOST:$HTTP_PORT
+iptables -t nat -A PREROUTING --in-interface $AP_INTERFACE --protocol tcp -j DNAT --to-destination $LOCALHOST:$HTTP_PORT
+#iptables -t nat -A PREROUTING --in-interface $AP_INTERFACE --protocol tcp --dport 80 -j REDIRECT --to-port $HTTP_PORT
