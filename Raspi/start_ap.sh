5b5435a8622d29b52aae656dd087bad3cc5a472d
c <snakecharmr1024@gmail.com>
2015-11-10 01:41:47 -0800

    Ok the wispr thing still doesn't work, BUT dnsmasq now redirects all http requests to localhost, so going to pizza.com or peiaiaaiaia.org sends you to the totem, which I think is acceptable, but it doesn't redirect https requests because of the lack of the appropriate digital signature. Will continue to work on the wispr thing. I think it's just a matter of time.

--- Raspi/start_ap.sh
+++ Raspi/start_ap.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-IPT=iptables
 LOCALHOST=10.0.19.84
-TOTEM_LOCALHOST=10.0.0.1
 AP_INTERFACE=wlan4
 #ifconfig $AP_INTERFACE down up $LOCALHOST
 
 
-#service hostapd restart
-#service dnsmasq restart
 
 HTTP_PORT=3000
 
@@ -18,15 +14,5 @@ iptables -X
 iptables -t nat -F
 iptables -t nat -X
 
-# Accept everything by default
-#$IPT -t nat --policy PREROUTING ACCEPT
-#$IPT -t nat --policy POSTROUTING ACCEPT
-#$IPT -t nat --policy OUTPUT ACCEPT
 
-#$IPT -A FORWARD --protocol ALL --in-interface $AP_INTERFACE -j ACCEPT
-#$IPT -A FORWARD --in-interface $AP_INTERFACE -m state --state ESTABLISHED,RELATED -j ACCEPT
-
-
-iptables -t nat -A PREROUTING --in-interface $AP_INTERFACE --protocol udp -j DNAT --to-destination $LOCALHOST:$HTTP_PORT
-iptables -t nat -A PREROUTING --in-interface $AP_INTERFACE --protocol tcp -j DNAT --to-destination $LOCALHOST:$HTTP_PORT
-#iptables -t nat -A PREROUTING --in-interface $AP_INTERFACE --protocol tcp --dport 80 -j REDIRECT --to-port $HTTP_PORT
+iptables -t nat -A PREROUTING --in-interface $AP_INTERFACE --protocol tcp -j REDIRECT --dst $LOCALHOST --to-ports $HTTP_PORT
