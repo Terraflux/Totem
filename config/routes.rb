f5c1de03a6308ec0bf89a0bdab6a2063169d4088
c <snakecharmr1024@gmail.com>
2015-11-13 23:12:26 -0800

    BAM Wispr works on iPhone and Android now. Just needed to do one final redirect from a request for any file into the index.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     resources :scribbles, except: [:index]
   end
 
+  get '/*path' => "inscriptions#index", :code => 302, :via => [:get]
   root to: 'inscriptions#index'
 
 end
